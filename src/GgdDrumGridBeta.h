f8f8851d144377b8b395178fa41b87e9c95bb0aa
001null100 <128686360+001null100@users.noreply.github.com>
2026-08-28 21:52:26 +0300

    Beta 3: expose selection probability and drum transforms

--- src/GgdDrumGridBeta.h
+++ src/GgdDrumGridBeta.h
@@ -37,15 +37,20 @@ public:
     void resetZoom();
 
     int getSelectedCount() const { return static_cast<int>(selection.size()); }
+    int getSelectedProbability() const;
     void pollFallbackShortcuts(bool active);
 
     void selectAll();
     void copySelectionToClipboard();
     void pasteClipboard();
     void adjustSelectedVelocityBy(int delta);
+    void setSelectedVelocity(int velocity);
+    void setSelectedProbability(int probability);
     void adjustSelectedTimingBy(int deltaTicks);
     void quantizeSelected();
     void humanizeSelected();
+    void createFlamFromSelection();
+    void createDoubleFromSelection();
     void deleteSelected();
 
     bool keyPressed(const juce::KeyPress& key) override;
@@ -209,6 +214,7 @@ private:
     bool moveSelectionBy(int deltaTicks, int deltaRows, bool duplicate);
     bool nudgeSelection(int horizontal, int vertical, bool duplicate);
     void adjustSelectionVelocity(int delta);
+    void duplicateSelectionWithOffset(int deltaTicks, float velocityScale);
     void deleteSelection();
     void beginSelectionMove(const juce::MouseEvent& e, bool duplicate);
     void finishSelectionMove();
